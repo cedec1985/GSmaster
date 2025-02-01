77a67790b5fa519971832ac54c1353953179e10f
Cédric De Craim <cedricdecraim@msn.com>
2025-02-01 15:05:40 +0100

    remise

--- MyBackendApp/java/gs/java/fs/utils/BCryptUtils.java
+++ MyBackendApp/java/gs/java/fs/utils/BCryptUtils.java
@@ -1,13 +1,11 @@
 package java.gs.java.fs.utils;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 
 @Component
-@Configuration
 public class BCryptUtils {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
