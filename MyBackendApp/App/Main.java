91649fc027967bcbbc12848feafd8033a6dcbf78
Cédric De Craim <cedricdecraim@msn.com>
2025-02-01 15:20:42 +0100

    pack

--- MyBackendApp/App/Main.java
+++ MyBackendApp/App/Main.java
@@ -1,4 +1,4 @@
-package java.gs.java.fs;
+package 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
