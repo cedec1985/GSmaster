f9aae69500f40cac0b7ac0437759f04b75d73dcd
Cédric De Craim <cedricdecraim@msn.com>
2025-02-02 11:02:19 +0100

    valide

--- MyBackendApp/app/main/Main.java
+++ MyBackendApp/app/main/Main.java
@@ -1,4 +1,4 @@
-package app.main;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
