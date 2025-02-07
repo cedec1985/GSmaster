7c0a4c1d493082c2884513e86f4b6d3baca7630f
Cédric De Craim <cedricdecraim@msn.com>
2025-02-07 12:49:54 +0100

    valide

---
+++ .angular/cache/17.3.10/vite/deps/browser-STE3MIGH.js
@@ -0,0 +1,54 @@
+import {
+  Animation,
+  AnimationDriver,
+  AnimationEngine,
+  AnimationRenderer,
+  AnimationRendererFactory,
+  AnimationStyleNormalizer,
+  BaseAnimationRenderer,
+  NoopAnimationDriver,
+  NoopAnimationStyleNormalizer,
+  WebAnimationsDriver,
+  WebAnimationsPlayer,
+  WebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge,
+  camelCaseToDashCase,
+  containsElement,
+  createEngine,
+  getParentElement,
+  invokeQuery,
+  normalizeKeyframes,
+  validateStyleProperty,
+  validateWebAnimatableStyleProperty
+} from "./chunk-53BY2TJH.js";
+import "./chunk-C3BELRGL.js";
+import "./chunk-LUL6M5O6.js";
+import "./chunk-ZFFGU5H4.js";
+import "./chunk-664N5FMB.js";
+import "./chunk-JND6LT5A.js";
+import "./chunk-532FTKWE.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  AnimationDriver,
+  NoopAnimationDriver,
+  Animation as ɵAnimation,
+  AnimationEngine as ɵAnimationEngine,
+  AnimationRenderer as ɵAnimationRenderer,
+  AnimationRendererFactory as ɵAnimationRendererFactory,
+  AnimationStyleNormalizer as ɵAnimationStyleNormalizer,
+  BaseAnimationRenderer as ɵBaseAnimationRenderer,
+  NoopAnimationStyleNormalizer as ɵNoopAnimationStyleNormalizer,
+  WebAnimationsDriver as ɵWebAnimationsDriver,
+  WebAnimationsPlayer as ɵWebAnimationsPlayer,
+  WebAnimationsStyleNormalizer as ɵWebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge as ɵallowPreviousPlayerStylesMerge,
+  camelCaseToDashCase as ɵcamelCaseToDashCase,
+  containsElement as ɵcontainsElement,
+  createEngine as ɵcreateEngine,
+  getParentElement as ɵgetParentElement,
+  invokeQuery as ɵinvokeQuery,
+  normalizeKeyframes as ɵnormalizeKeyframes,
+  validateStyleProperty as ɵvalidateStyleProperty,
+  validateWebAnimatableStyleProperty as ɵvalidateWebAnimatableStyleProperty
+};
+//# sourceMappingURL=browser-STE3MIGH.js.map
