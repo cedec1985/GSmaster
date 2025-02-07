44bee1a4139f18b664708e1fd58527c994013825
Cédric De Craim <cedricdecraim@msn.com>
2025-02-07 11:03:26 +0100

    valide

--- Frontend/app/app.config.ts
+++ Frontend/app/app.config.ts
@@ -1,9 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes} from './app.routes';
-
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimations, provideNoopAnimations } from '@angular/platform-browser/animations';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { registerLocaleData } from '@angular/common';
@@ -15,9 +14,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(), 
-    provideAnimations(), 
     provideNoopAnimations(),
-    provideHttpClient(), provideAnimationsAsync('noop'), provideAnimationsAsync('noop')
+    provideHttpClient(), provideAnimationsAsync('noop'),
   ]
 };
 
