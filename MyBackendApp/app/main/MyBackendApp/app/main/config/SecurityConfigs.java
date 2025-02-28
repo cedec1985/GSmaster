a30cd2346eba4773d61e8595b397a77d49a8293c
Cédric De Craim <cedricdecraim@msn.com>
2025-02-28 21:03:28 +0100

    securityconfig

--- MyBackendApp/app/main/MyBackendApp/app/main/config/SecurityConfigs.java
+++
@@ -1,7 +0,0 @@
-package MyBackendApp.app.main.config;
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
-public interface SecurityConfigs {
-    void configure(AuthenticationManagerBuilder auth) throws Exception;
-}
