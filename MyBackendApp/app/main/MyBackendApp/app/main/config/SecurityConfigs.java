5181fc76d7faad4d29cbdefe8648eab26886e883
Cédric De Craim <cedricdecraim@msn.com>
2025-03-04 17:51:21 +0100

    ok

--- MyBackendApp/app/main/MyBackendApp/app/main/config/SecurityConfigs.java
+++ MyBackendApp/app/main/MyBackendApp/app/main/config/SecurityConfigs.java
@@ -1,7 +1,6 @@
 package MyBackendApp.app.main.config;
 
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
