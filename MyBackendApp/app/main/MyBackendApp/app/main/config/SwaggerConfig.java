25d20baa0eb93acebf5f24f83504bc4e76c18a8b
Cédric De Craim <cedricdecraim@msn.com>
2025-03-04 15:46:33 +0100

    config

--- MyBackendApp/app/main/MyBackendApp/app/main/config/SwaggerConfig.java
+++ MyBackendApp/app/main/MyBackendApp/app/main/config/SwaggerConfig.java
@@ -3,9 +3,6 @@ package MyBackendApp.app.main.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.base.Predicate;
-
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
