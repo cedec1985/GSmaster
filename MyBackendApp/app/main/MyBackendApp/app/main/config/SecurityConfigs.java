260ec1780012bb3b9b16eff204cec059ac983edc
Cédric De Craim <cedricdecraim@msn.com>
2025-03-05 14:45:46 +0100

    config

--- MyBackendApp/app/main/MyBackendApp/app/main/config/SecurityConfigs.java
+++ MyBackendApp/app/main/MyBackendApp/app/main/config/SecurityConfigs.java
@@ -3,8 +3,6 @@ package MyBackendApp.app.main.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
 
 import MyBackendApp.app.main.bll.impl.UserDetailsServiceImpl;
 import MyBackendApp.app.main.dal.UserInfoRepository;
@@ -24,7 +21,6 @@ import MyBackendApp.app.main.dal.UserInfoRepository;
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 
-@Component
     public class SecurityConfigs extends UserDetailsServiceImpl{
         
         public SecurityConfigs(UserInfoRepository userinfoRepository) {
