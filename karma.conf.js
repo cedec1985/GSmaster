abb82c981d9264b73b301ffb0487a352a30df475
Cédric De Craim <cedricdecraim@msn.com>
2025-07-19 18:19:15 +0200

    Karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'Frontend/app/pipe/*.ts',
+      'Frontend/app/pipes/*.ts',
       'Frontend/app/tests/*.ts'
     ],
 
